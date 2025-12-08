9b9c3c0dd07f65dce66967eef76bad16b2297ed1
William <bill1102@William.localdomain>
2025-12-08 00:08:47 -0500

    Project 3 implementation

--- parsing/parse.pl
+++ parsing/parse.pl
@@ -3,7 +3,37 @@
 % Your code here:
 %%%%%%%%%%%%%%%%%
 
-parse(X) :- ????
+parse(X) :- lines(X, []).
+
+lines(Input, Rest) :-
+    line(Input, Rest1),
+    (   Rest1 = [';'|Rest2] ->
+        lines(Rest2, Rest)
+    ;   Rest1 = Rest
+    ).
+
+line(Input, Rest) :-
+    num(Input, Rest1),
+    (   Rest1 = [','|Rest2] ->
+        line(Rest2, Rest)
+    ;   Rest1 = Rest
+    ).
+
+num(Input, Rest) :-
+    digit(Input, Rest1),
+    (   digit(Rest1, Rest2) ->
+        num_cont(Rest2, Rest)
+    ;   Rest1 = Rest
+    ).
+
+num_cont(Input, Rest) :-
+    (   digit(Input, Rest1) ->
+        num_cont(Rest1, Rest)
+    ;   Input = Rest
+    ).
+
+digit([H|T], T) :-
+    member(H, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']).
 
 % Example execution:
 % ?- parse(['3', '2', ',', '0', ';', '1', ',', '5', '6', '7', ';', '2']).
--- search/search.pl
+++ search/search.pl
@@ -2,4 +2,47 @@
 % Your code goes here:
 %%%%%%%%%%%%%%%%%%%%%%
 
-search(Actions) :- ???
+search(Actions) :-
+    initial(Start),
+    bfs([[state(Start, []), []]], Actions).
+
+bfs([[state(Room, _Keys), Actions] | _], FinalActions) :-
+    treasure(Room),
+    reverse(Actions, FinalActions).
+
+bfs([[state(Room, Keys), Actions] | Rest], Solution) :-
+    \+ treasure(Room),
+    findall([state(NewRoom, NewKeys), [move(Room, NewRoom) | Actions]],
+            can_move(Room, NewRoom, Keys, NewKeys), NewStates),
+    append(Rest, NewStates, Queue),
+    bfs(Queue, Solution).
+
+can_move(From, To, Keys, NewKeys) :-
+    door(From, To),
+    NewKeys = Keys.
+
+can_move(From, To, Keys, NewKeys) :-
+    door(To, From),
+    NewKeys = Keys.
+
+can_move(From, To, Keys, NewKeys) :-
+    locked_door(From, To, Color),
+    member(Color, Keys),
+    NewKeys = Keys.
+
+can_move(From, To, Keys, NewKeys) :-
+    locked_door(To, From, Color),
+    member(Color, Keys),
+    NewKeys = Keys.
+
+can_move(From, To, Keys, NewKeys) :-
+    key(To, Color),
+    door(From, To),
+    \+ member(Color, Keys),
+    NewKeys = [Color | Keys].
+
+can_move(From, To, Keys, NewKeys) :-
+    key(To, Color),
+    door(To, From),
+    \+ member(Color, Keys),
+    NewKeys = [Color | Keys].
